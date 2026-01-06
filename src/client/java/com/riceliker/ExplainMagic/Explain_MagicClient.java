015de958dcea6b7f3b135043dbd8bf65b98dcafe
riceliker <riceliker@outlook.com>
2026-01-06 14:38:17 +0800

    init

---
+++ src/client/java/com/riceliker/ExplainMagic/Explain_MagicClient.java
@@ -0,0 +1,10 @@
+package com.riceliker.ExplainMagic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Explain_MagicClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
